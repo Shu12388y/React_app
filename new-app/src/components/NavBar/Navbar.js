6a7311feaa6917d55ddceed2ceb3bd0ac1fd83ae
shubham <spl.sp999@gmail.com>
2023-01-24 23:30:38 +0530

    add

---
+++ new-app/src/components/NavBar/Navbar.js
@@ -0,0 +1,21 @@
+import'./NavBar.css'
+
+function navbar(){
+    return(
+        <div className='upper__parent'>
+        <nav>
+            <ul className='nav__bar'>
+                <li className='list_name'>
+                    home
+                </li>
+                <li>
+                    contact us
+                </li>
+                <li>
+                    about us
+                </li>
+            </ul>
+        </nav>
+        </div>
+    )
+}
